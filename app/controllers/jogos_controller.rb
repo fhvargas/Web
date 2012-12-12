36163924ef1a323872c17d374aac056f8bbdb2cb
jdmoeller <juliano_moeller@terra.com.br>
2012-12-12 09:37:44 -0200

    Update app/controllers/jogos_controller.rb

--- app/controllers/jogos_controller.rb
+++ app/controllers/jogos_controller.rb
@@ -47,7 +47,7 @@ class JogosController < ApplicationController
         format.html { redirect_to @jogo, :notice => 'Jogo was successfully created.' }
         format.json { render :json => @jogo, :status => :created, :location => @jogo }
       else
-        format.html { render :action => "new" }
+        format.html { render :action => "New" }
         format.json { render :json => @jogo.errors, :status => :unprocessable_entity }
       end
     end
@@ -63,7 +63,7 @@ class JogosController < ApplicationController
         format.html { redirect_to @jogo, :notice => 'Jogo was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render :action => "edit" }
+        format.html { render :action => "Edit" }
         format.json { render :json => @jogo.errors, :status => :unprocessable_entity }
       end
     end
